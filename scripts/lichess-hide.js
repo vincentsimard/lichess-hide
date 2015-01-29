844cfd8e9207369f6ac359d72a88f84a6b3b3915
Vincent Simard <vincent@vincentsimard.com>
2015-01-29 13:02:10 -0500

    Click toggles x

--- scripts/lichess-hide.js
+++ scripts/lichess-hide.js
@@ -1,27 +1,29 @@
 'use strict';
 
-// var lichess = {};
-
-// lichess.el = document.querySelector('#lichess');
-// lichess.elTable = lichess.el.querySelector('.lichess_ground .table');
-// lichess.elBoard = lichess.el.querySelector('.lichess_board');
-// lichess.elPlayerClock = lichess.el.querySelector('.clock_bottom');
-
 function createButton() {
   var div = document.createElement('div');
   var a = document.createElement('a');
-  var content = document.createTextNode('Hide stuff');
+  var content = document.createTextNode('Hide information');
   var previousElement = document.getElementById('message_notifications_parent');
 
   div.id = 'lichess-hide';
   a.href = '#';
   a.className = 'toggle';
+
   a.appendChild(content);
   div.appendChild(a);
 
+  a.addEventListener('click', toggleHide);
+
   previousElement.parentNode.insertBefore(div, previousElement.nextSibling);
+
+}
+
+function toggleHide(event) {
+  event.preventDefault();
+
+  this.classList.toggle('on');
 }
 
 createButton();
 
-console.log('lichess-hide');
