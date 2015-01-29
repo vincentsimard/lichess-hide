93a04d6817866fb15d5dbb26293b7814d168e10a
Vincent Simard <vincent@vincentsimard.com>
2015-01-29 14:51:48 -0500

    Removing names from document.title

--- scripts/lichess-hide.js
+++ scripts/lichess-hide.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var NAME = 'lichess-hide';
+var TITLE = document.title;
 
 function createButton() {
   var div = document.createElement('div');
   var a = document.createElement('a');
   var content = document.createTextNode('Hide information');
   var previousElement = document.getElementById('message_notifications_parent');
+  var saved = localStorage.getItem(NAME);
 
   div.id = NAME;
   a.href = '#';
@@ -19,16 +21,22 @@ function createButton() {
 
   previousElement.parentNode.insertBefore(div, previousElement.nextSibling);
 
-  if (localStorage.getItem(NAME)) { a.click(); }
+  if (saved && saved !== 'false') { a.click(); }
 }
 
 function toggleHide(event) {
   event.preventDefault();
 
+  var isOn;
+
   this.classList.toggle('on');
   document.body.classList.toggle(NAME);
 
-  localStorage.setItem(NAME, this.classList.contains('on'));
+  isOn = this.classList.contains('on');
+
+  document.title = isOn ? 'lichess.org' : TITLE;
+
+  localStorage.setItem(NAME, isOn);
 }
 
 createButton();
